35a0d85c1eabb350fe78d0b7c599f50502a1004c
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-09-14 18:52:35 +0530

    Signup hook was hooked with the signup page to register new users.

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { useSignup } from "./../../hooks/useSignup";
 import styles from "./Signup.module.css";
 
 export default function Signup() {
@@ -7,10 +7,11 @@ export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { error, isPending, signup } = useSignup();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log(email, password, displayName);
+    signup(email, password, displayName);
   };
 
   return (
@@ -44,7 +45,10 @@ export default function Signup() {
         />
       </label>
 
-      <button className="btn">Sign Up</button>
+      {!isPending && <button className="btn">Sign Up</button>}
+
+      {isPending && <button className="btn" disabled>Loading...</button>}
+      {error && <p>{error}</p>}
     </form>
   );
 }
