c1a2575886f05a5974b19eed3a4db7814304eeb7
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-09-15 05:20:33 +0530

    Cleanup functions were added to useLogin and useSignup hooks.

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,10 +1,12 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const [isCancelled, setIsCancelled] = useState(false);
+
   const { dispatch } = useAuthContext();
 
   const logout = async () => {
@@ -15,14 +17,28 @@ export const useLogout = () => {
     try {
       await projectAuth.signOut();
       dispatch({ type: "LOGOUT" });
-      setIsPending(false);
-      setError(null);
+
+      // update state
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
     } catch (err) {
-      console.log(err.message);
-      setError(err.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      }
     }
   };
+
+  // Any time we're using a hook like this, which is going to update states in a component, then we should use a clean-up function.
+  // In fetch api we have abort controller to abort the fetch. In firestore real time data, we used unsubscribe function which we can call in the cleanup function.
+  // But we cannot use any of those here. So, we have to do a bit more of manual approach. -> ([isCancelled, setIsCancelled])
+  // Clean up function should be in the useEffect() hook.
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
 
   return { logout, error, isPending };
 };
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const [isCancelled, setIsCancelled] = useState(false);
+
   const { dispatch } = useAuthContext();
 
   const signup = async (email, password, displayName) => {
@@ -29,14 +31,27 @@ export const useSignup = () => {
       // dispatch login action
       dispatch({ type: "LOGIN", payload: res.user });
 
-      setIsPending(false);
-      setError(null);
+      // update state
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
     } catch (err) {
-      console.log(err.message);
-      setError(err.message);
-      setIsPending(false);
+      if (!isCancelled) {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      }
     }
   };
+
+  // Any time we're using a hook like this, which is going to update states in a component, then we should use a clean-up function.
+  // In fetch api we have abort controller to abort the fetch. In firestore real time data, we used unsubscribe function which we can call in the cleanup function.
+  // But we cannot use any of those here. So, we have to do a bit more of manual approach. -> ([isCancelled, setIsCancelled])
+  // Clean up function should be in the useEffect() hook.
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
 
   return {
     error,
