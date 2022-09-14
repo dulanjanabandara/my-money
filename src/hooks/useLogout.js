6c14367377be6ba34479bf90f9956d204a0c6ddc
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-09-15 03:58:37 +0530

    Logout hook was created and added logout case in the auth context reducer.

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,28 @@
+import { useState, useContext } from "react";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    // sign out the user
+    try {
+      await projectAuth.signOut();
+      dispatch({ type: "LOGOUT" });
+      setIsPending(false);
+      setError(null);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { logout, error, isPending };
+};
