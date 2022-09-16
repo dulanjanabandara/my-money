87ef01343cb745b1aab2d1bab9107930a56d0803
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-09-17 01:47:45 +0530

    Conditional showing of login/register and logout links were implemented.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,39 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "./../../hooks/useAuthContext";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>MyMoney</li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout} >Logout</button>
-        </li>
+
+        {!user && ( // fragment is used below because when we're using a template like this, we have to have one root element.
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hello, {user.user.displayName}</li>
+            <p>{console.log(user.user.displayName)}</p>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
