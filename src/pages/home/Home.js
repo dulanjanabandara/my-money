98c0019865fef4853c62019b77eae1997492fdc1
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-09-18 11:13:09 +0530

    useFirestore hook was used to for creating transaction docs and added a property for the currently logged in user and added form clearance after the form is submitted.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
+import { useAuthContext } from "./../../hooks/useAuthContext";
 import TransactionForm from "./TransactionForm/TransactionForm";
 import styles from "./Home.module.css";
 
 export default function Home() {
+  const { user } = useAuthContext();
   return (
     <div className={styles.container}>
       <div className={styles.content}>transaction list</div>
       <div className={styles.sidebar}>
-        <TransactionForm></TransactionForm>
+        <TransactionForm uid={user.uid}></TransactionForm>
       </div>
     </div>
   );
